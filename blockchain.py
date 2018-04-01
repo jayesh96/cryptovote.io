d6a62f2258a5f1e517064f6cc9d3919ee94c1cd9
Jayesh Bidani <jayesh.bidani@gmail.com>
2018-04-02 00:04:25 +0530

    Improved reponse time

--- blockchain.py
+++ blockchain.py
@@ -209,4 +209,4 @@ if __name__ == '__main__':
     parser.add_argument('-p', '--port', default=5000, type=int, help='port to listen on')
     args = parser.parse_args()
     port = args.port
-    app.run(host='0.0.0.0', port=port)
+    app.run(host='0.0.0.0', port=port, threaded=True)
